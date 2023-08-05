c0a35d8d481d73a6aaf2ddc9a6e553d0a4484dae
Victor Saccucci <victorsaccucci@hotmail.com>
2023-08-05 16:49:18 -0300

    Get method get description from game id

--- main.go
+++ main.go
@@ -1,89 +1,97 @@
 package main
 
 import (
-	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+
+	"modulo-go-project/database"
 )
 
 func main() {
 
-	db, err := sql.Open("mysql", "root:admin@tcp(localhost:3306)/gowebprojectdb")
-
+	db, err := database.OpenDB()
 	if err != nil {
-		fmt.Println("error validating sql.Open arguments")
-		panic(err.Error())
+		log.Fatal(err)
 	}
-	defer db.Close()
+	defer database.CloseDB(db)
 
+	// Testar a conexão com o banco de dados
 	err = db.Ping()
 	if err != nil {
-		fmt.Println("error verifying connection with db.Ping")
-		panic(err.Error())
+		log.Fatal("Erro ao testar a conexão com o banco de dados:", err)
 	}
-	insert, err := db.Query("INSERT INTO Game (Titulo, Ano, Genero, GameList) VALUES ('The Witcher 3', 2015, 'RPG', 4);")
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/game/{id}", handleGetGameByID)
+	router.HandleFunc("/games", handleAllGames)
+	router.HandleFunc("/description/gameId/{id}", handleGetDescriptionByIDgame)
+
+	port := ":8080"
+	fmt.Printf("Servidor rodando em http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, router))
+
+}
+func handleAllGames(w http.ResponseWriter, r *http.Request) {
+	games, err := database.GetAllGames()
 	if err != nil {
-		panic(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
-	defer insert.Close()
-	fmt.Println("Successful Connection to Database!")
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(games)
+
 }
 
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-// 	"strconv"
+func handleGetGameByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idGame := vars["id"]
 
-// 	"github.com/gorilla/mux"
+	id, err := strconv.Atoi(idGame)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
 
-// 	"database/sql"
-// 	"modulo-go-project/database"
-// )
+	game, err := database.GetGameByID(id)
+	if err != nil {
+		if err == database.ErrNoRows {
+			http.Error(w, "Game not found", http.StatusNotFound)
+		} else {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+		return
+	}
 
-// func main() {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(game)
+}
+func handleGetDescriptionByIDgame(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idGame := vars["id"]
 
-// 	// Abrir conexão com o banco de dados
-// 	db, err := database.OpenDB()
-// 	if err != nil {
-// 		log.Fatal("Erro ao estabelecer a conexão com o banco de dados:", err)
-// 	}
-// 	defer database.CloseDB(db) // Fechar a conexão no final do programa
+	id, err := strconv.Atoi(idGame)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
 
-// 	// Verificar se a conexão foi bem sucedida
-// 	if err = db.Ping(); err != nil {
-// 		log.Fatal("Erro ao testar a conexão com o banco de dados:", err)
-// 	}
+	gameList, err := database.GetDescriptionListByGameID(id)
+	if err != nil {
+		if err == database.ErrNoRows {
+			http.Error(w, "Game not found", http.StatusNotFound)
+		} else {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+		return
+	}
 
-// 	// Resto do seu código aqui...
-
-// 	port := ":8080"
-// 	fmt.Printf("Servidor rodando em http://localhost%s\n", port)
-// 	log.Fatal(http.ListenAndServe(port, nil))
-// }
-
-// func handleGetGameByID(w http.ResponseWriter, r *http.Request) {
-// 	varHttp := mux.Vars(r)
-// 	idGame := varHttp["id"]
-
-// 	id, err := strconv.Atoi(idGame)
-// 	if err != nil {
-// 		http.Error(w, "Invalid ID", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	game, err := database.GetGameByID(id)
-// 	if err != nil {
-// 		if err == sql.ErrNoRows {
-// 			http.Error(w, "Game not found", http.StatusNotFound)
-// 		} else {
-// 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-// 		}
-// 		return
-// 	}
-
-// 	w.Header().Set("Content-Type", "application/json")
-// 	json.NewEncoder(w).Encode(game)
-// }
+	w.Header().Set("Content-Type", "application/json") // Corrigido: "Content-Type" em vez de "Content-Typer"
+	json.NewEncoder(w).Encode(gameList)
+}
