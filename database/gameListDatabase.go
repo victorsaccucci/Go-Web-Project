57062bbec776d9841e5486a6f748008dba300b64
Victor Saccucci <victorsaccucci@hotmail.com>
2023-08-05 20:52:12 -0300

    Get GameList By ID

--- database/gameListDatabase.go
+++ database/gameListDatabase.go
@@ -6,6 +6,24 @@ import (
 	"modulo-go-project/models"
 )
 
+func GetGameListByID(id int) (models.GameList, error) {
+	db, err := OpenDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer CloseDB(db)
+
+	var gamelist models.GameList
+
+	query := "SELECT gamelist.id, gamelist.descricao from gamelist where id = ?"
+	err = db.QueryRow(query, id).Scan(&gamelist.Id, &gamelist.Descricao)
+
+	if err == sql.ErrNoRows {
+		return gamelist, err
+	}
+	return gamelist, nil
+}
+
 func GetDescriptionListByGameID(id int) (models.GameList, error) {
 	db, err := OpenDB()
 	if err != nil {
@@ -19,9 +37,7 @@ func GetDescriptionListByGameID(id int) (models.GameList, error) {
 	FROM game
 	INNER JOIN gamelist ON
 	game.gamelist = gamelist.id
-	WHERE game.idgame = ?;
-	
-	`
+	WHERE game.idgame = ?;`
 
 	err = db.QueryRow(query, id).Scan(&gamelist.Id, &gamelist.Descricao)
 
